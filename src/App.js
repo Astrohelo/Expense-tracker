d9622750e4fcd51855a6db9600b82a42cbc50eaa
Axel Várai <58273598+Astrohelo@users.noreply.github.com>
2023-05-25 16:49:49 +0200

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpensHandler = (expense) => {
+    setExpenses((previous) => {
+      return [expense, ...previous];
+    });
+  };
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpensHandler} />
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDatahandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditing = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDatahandler}
+          onCancel={stopEditing}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
